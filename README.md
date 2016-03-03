d6badce46d4dda94ef38d3f5e9a3aabebec15ee4
charlesguo <charles.j.guo@gmail.com>
2016-03-03 16:01:52 +0800

    Updates README.md and cleans-up app.js

--- README.md
+++ README.md
@@ -8,9 +8,21 @@ Tech Memory is built using the following technologies:
 1. [Font-Awesome](https://fortawesome.github.io/Font-Awesome/) for the icons
 
 ## Approach Taken
-1. Create an array to store all the icons.
+1. Create an array to store all the icons. There are 12 pairs.
 1. Randomly shuffle the icons in the array.
-1.
+1. For each icon in the array, create each tile as a div within the "memory_board" div.
+1. Allow each user to click only up to 2 tiles at a time.
+1. If the 2 tiles match, add the player's colour to them, update the score and keep track of the number of tiles uncovered. Let the same player continue playing.
+1. If the 2 tiles do not match, switch players. Cover back the tiles after 700 ms.
+1. When all the tiles have been clicked, present the score in a dialog box and reset the game.
 
-### Link to the Game
+## Instructions
+1. When the page loads, Player 1 always starts first as indicated by his label being shown in red. Player 1 can click any 2 tiles to display the respective icons.
+1. If this results in a match, Player 1 continues playing.
+1. If not, it is now Player 2's turn. Similarly, Player 2 keeps his/her turn as long as he/she continues uncovering matches.
+1. The relevant scores are updated during gameplay.
+1. If you want to reset the game at anytime, click on the reset button.
+1. The game ends when all tiles have been uncovered. 
+
+## Link to the Game
 * [Project 1 - Memory Game](http://charlesguo.github.io/memory-game)
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /*global $, document*/
-$(document).ready(newBoard);
 
 var Array;
 var memory_array = ['<i class="fa fa-instagram"></i>',
@@ -26,7 +25,7 @@ var memory_array = ['<i class="fa fa-instagram"></i>',
                     '<i class="fa fa-facebook-square"></i>',
                     '<i class="fa fa-google"></i>',
                     '<i class="fa fa-google"></i>'];
-// var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+
 var memory_values = [];
 var memory_tiles = [];
 var tiles_clicked = 0;
@@ -38,19 +37,37 @@ var tile_1;
 var tile_2;
 var end_message;
 
-
-$("#resetButton").on("click", newBoard);
-$("#memory_board").on('click', updateScore);
-
-
-Array.prototype.memory_tile_shuffle = function(){
-    var i = this.length, j, temp;
-    while(--i > 0){
-        j = Math.floor(Math.random() * (i+1));
+Array.prototype.memory_tile_shuffle = function () {
+    var i;
+    var j;
+    var temp;
+    for (i = this.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
         temp = this[j];
         this[j] = this[i];
         this[i] = temp;
     }
+}
+
+function originalHeaders() {
+    $("#player1-header").css("color", "#d88189");
+    $("#player2-header").css("color", "black");
+}
+
+function swapHeaders() {
+    $("#player1-header").css("color", "black");
+    $("#player2-header").css("color", "#d88189");
+}
+
+
+function clearArrays() {
+    memory_values = [];
+    memory_tiles = [];
+}
+
+function updateScore() {
+  $("#p1-num-pairs").html(score_player_1);
+  $("#p2-num-pairs").html(score_player_2);
 }
 
 function newBoard(){
@@ -74,7 +91,6 @@ function newBoard(){
 
   $('#memory_board > div').on('click', function () {
     tiles_clicked ++;
-    console.log(tiles_clicked);
 
     if (tiles_clicked <= 2) {
         $(this).css("background-color", "#b5d881");
@@ -88,7 +104,6 @@ function newBoard(){
     if (tiles_clicked === 2) {
         if(memory_values[0] == memory_values[1]){
   				tiles_flipped += 2;
-
 
           if(current_player_1) {
               score_player_1 ++;
@@ -154,22 +169,7 @@ function flip2Back() {
       clearArrays();
 }
 
-function originalHeaders() {
-    $("#player1-header").css("color", "#d88189");
-    $("#player2-header").css("color", "black");
-}
+$(document).ready(newBoard);
 
-function swapHeaders() {
-    $("#player1-header").css("color", "black");
-    $("#player2-header").css("color", "#d88189");
-}
-
-function clearArrays() {
-    memory_values = [];
-    memory_tiles = [];
-}
-
-function updateScore() {
-  $("#p1-num-pairs").html(score_player_1);
-  $("#p2-num-pairs").html(score_player_2);
-}
+$("#resetButton").on("click", newBoard);
+$("#memory_board").on('click', updateScore);
