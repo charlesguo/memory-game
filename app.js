e2dcec299db6642c21837db97df1028d47f72f5a
charlesguo <charles.j.guo@gmail.com>
2016-03-03 10:02:02 +0800

    adds full game functionality

--- app.js
+++ app.js
@@ -1,9 +1,47 @@
-// Based on the following video tutorial:
-// http://www.youtube.com/watch?v=c_ohDPWmsM0
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+/*global $, document*/
+$(document).ready(newBoard);
+
+var Array;
+var memory_array = ['<i class="fa fa-instagram"></i>',
+                    '<i class="fa fa-instagram"></i>',
+                    '<i class="fa fa-reddit"></i>',
+                    '<i class="fa fa-reddit"></i>',
+                    '<i class="fa fa-tumblr"></i>',
+                    '<i class="fa fa-tumblr"></i>',
+                    '<i class="fa fa-amazon"></i>',
+                    '<i class="fa fa-amazon"></i>',
+                    '<i class="fa fa-youtube-square"></i>',
+                    '<i class="fa fa-youtube-square"></i>',
+                    '<i class="fa fa-yelp"></i>',
+                    '<i class="fa fa-yelp"></i>',
+                    '<i class="fa fa-spotify"></i>',
+                    '<i class="fa fa-spotify"></i>',
+                    '<i class="fa fa-pinterest-square"></i>',
+                    '<i class="fa fa-pinterest-square"></i>',
+                    '<i class="fa fa-github-alt"></i>',
+                    '<i class="fa fa-github-alt"></i>',
+                    '<i class="fa fa-apple"></i>',
+                    '<i class="fa fa-apple"></i>',
+                    '<i class="fa fa-facebook-square"></i>',
+                    '<i class="fa fa-facebook-square"></i>',
+                    '<i class="fa fa-google"></i>',
+                    '<i class="fa fa-google"></i>'];
+// var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
 var memory_values = [];
-var memory_tile_ids = [];
+var memory_tiles = [];
+var tiles_clicked = 0;
 var tiles_flipped = 0;
+var current_player_1;
+var score_player_1 = 0;
+var score_player_2 = 0;
+var tile_1;
+var tile_2;
+var end_message;
+
+
+$("#resetButton").on("click", newBoard);
+$("#memory_board").on('click', updateScore);
+
 
 Array.prototype.memory_tile_shuffle = function(){
     var i = this.length, j, temp;
@@ -16,53 +54,122 @@ Array.prototype.memory_tile_shuffle = function(){
 }
 
 function newBoard(){
-	tiles_flipped = 0;
-	var output = '';
-    memory_array.memory_tile_shuffle();
+  $('#memory_board').html("");
+  tiles_flipped = 0;
+  current_player_1 = true;
+  score_player_1 = 0;
+  score_player_2 = 0;
+  originalHeaders();
+
+  memory_array.memory_tile_shuffle();
+
+
 	for(var i = 0; i < memory_array.length; i++){
-		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+    var div = document.createElement('div')
+    var idValue = 'tile' + i
+    div.setAttribute('id', idValue)
+    div.setAttribute('data-icon', memory_array[i])
+    document.getElementById('memory_board').appendChild(div)
 	}
-	document.getElementById('memory_board').innerHTML = output;
+
+  $('#memory_board > div').on('click', function () {
+    tiles_clicked ++;
+    console.log(tiles_clicked);
+
+    if (tiles_clicked <= 2) {
+        $(this).css("background-color", "#b5d881");
+        var icon = $(this).attr('data-icon');
+        $(this).html(icon);
+
+        memory_values.push(icon);
+        memory_tiles.push($(this));
+    }
+
+    if (tiles_clicked === 2) {
+        if(memory_values[0] == memory_values[1]){
+  				tiles_flipped += 2;
+
+
+          if(current_player_1) {
+              score_player_1 ++;
+              memory_tiles[0].css("background-color", "#d081d8");
+              memory_tiles[1].css("background-color", "#d081d8");
+          } else {
+              score_player_2 ++;
+              memory_tiles[0].css("background-color", "#81b5d8");
+              memory_tiles[1].css("background-color", "#81b5d8");
+          }
+
+          clearArrays();
+          tiles_clicked = 0;
+
+          if(tiles_flipped == memory_array.length){
+            updateScore();
+
+            if (score_player_1 > score_player_2) {
+                end_message = "Player 1 won with " + score_player_1 + " pairs!";
+            } else if (score_player_1 < score_player_2) {
+                end_message = "Player 2 won with " + score_player_2 + " pairs!";
+            } else {
+                end_message = "It's a tie!";
+            }
+            alert(end_message);
+            alert("Board cleared... generating new board");
+
+  					newBoard();
+  				}
+
+        } else {
+
+          setTimeout(flip2Back, 700);
+
+        }
+
+      }
+
+  })
+
 }
 
-function memoryFlipTile(tile,val){
-	if(tile.innerHTML == "" && memory_values.length < 2){
-		tile.style.background = '#FFF';
-		tile.innerHTML = val;
-		if(memory_values.length == 0){
-			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
-		} else if(memory_values.length == 1){
-			memory_values.push(val);
-			memory_tile_ids.push(tile.id);
-			if(memory_values[0] == memory_values[1]){
-				tiles_flipped += 2;
-				// Clear both arrays
-				memory_values = [];
-            	memory_tile_ids = [];
-				// Check to see if the whole board is cleared
-				if(tiles_flipped == memory_array.length){
-					alert("Board cleared... generating new board");
-					document.getElementById('memory_board').innerHTML = "";
-					newBoard();
-				}
-			} else {
-				function flip2Back(){
-				    // Flip the 2 tiles back over
-				    var tile_1 = document.getElementById(memory_tile_ids[0]);
-				    var tile_2 = document.getElementById(memory_tile_ids[1]);
-				    tile_1.style.background = 'url(tile_bg.jpg) no-repeat';
-            	    tile_1.innerHTML = "";
-				    tile_2.style.background = 'url(tile_bg.jpg) no-repeat';
-            	    tile_2.innerHTML = "";
-				    // Clear both arrays
-				    memory_values = [];
-            	    memory_tile_ids = [];
-				}
-				setTimeout(flip2Back, 700);
-			}
-		}
-	}
+function flip2Back() {
+      tiles_clicked = 0;
+
+      // Flip the 2 tiles back over
+      memory_tiles[0].css("background-color", "#d8d081");
+      memory_tiles[1].css("background-color", "#d8d081");
+      memory_tiles[0].html("");
+      memory_tiles[1].html("");
+
+      // Switch player
+      current_player_1 = !current_player_1;
+
+      // show whose turn is next
+      if(current_player_1) {
+          originalHeaders();
+        } else {
+          swapHeaders();
+        }
+
+      // Clear both arrays
+      clearArrays();
 }
 
-newBoard();
+function originalHeaders() {
+    $("#player1-header").css("color", "#d88189");
+    $("#player2-header").css("color", "black");
+}
+
+function swapHeaders() {
+    $("#player1-header").css("color", "black");
+    $("#player2-header").css("color", "#d88189");
+}
+
+function clearArrays() {
+    memory_values = [];
+    memory_tiles = [];
+}
+
+function updateScore() {
+  $("#p1-num-pairs").html(score_player_1);
+  $("#p2-num-pairs").html(score_player_2);
+}
